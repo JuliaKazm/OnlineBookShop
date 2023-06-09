4f5417fe261425fb0360c5976eb78e806e51aa4b
JuliaKazm <julia.kazmina@gmail.com>
2023-06-09 14:18:31 -0400

    JAXB

--- OnlineBookShop/src/main/java/jaxb/JAXBMain.java
+++ OnlineBookShop/src/main/java/jaxb/JAXBMain.java
@@ -3,7 +3,6 @@ package jaxb;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Unmarshaller;
-import jaxb.DataWrapper;
 import models.Customers;
 import models.Orders;
 import models.Payments;
